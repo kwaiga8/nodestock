656cd06e4b0714a2b70359ea6c6dce2c0701dc98
argentum <kwaiga8@gmail.com>
2020-06-06 16:49:27 -0400

    this terrible handlebars somehow works

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 5000;
-const otherstuff = "hello this is other stuff, hugly powerfull";
+
 
 //Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+const otherstuff = "hello this is other stuff, hugly powerfull";
 
 // set handlebars routes
 app.get('/', function (req, res) {
@@ -16,7 +18,10 @@ app.get('/', function (req, res) {
         stuff: otherstuff
     });
 });
-
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
 
 //Set static folder
 app.use( express.static(path.join(__dirname, 'public')));
